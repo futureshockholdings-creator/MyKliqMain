4c13242b105570c05f136d6fd7c37acf0d331a45
futureshockholdings-creator <futureshockholdings@gmail.com>
2025-10-28 12:27:52 -0400

    Amplify file added

---
+++ client/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist/public")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/public/index.html"));
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
