ea3ed1f4ed7b6b8825c293dcc44da760233431bb
futureshockholdings-creator <futureshockholdings@gmail.com>
2025-12-04 03:16:22 -0500

    Production Setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,34 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
+// export default defineConfig({
+//   base: "./",
+//   plugins: [
+//     react(),
+//   ],
+
+//     resolve: {
+//     alias: {
+//       "@": path.resolve(import.meta.dirname, "client", "src"),
+//       "@shared": path.resolve(import.meta.dirname, "shared"),
+//       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+//     },
+//   },
+//   root: path.resolve(import.meta.dirname, "client"),
+//   build: {
+//     outDir: path.resolve(import.meta.dirname, "dist"),
+//     emptyOutDir: true,
+//   },
+//   server: {
+//     fs: {
+//       strict: true,
+//       deny: ["**/.*"],
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -8,17 +39,23 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-
-    resolve: {
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(import.meta.dirname, "client", "src"),
+  //     "@shared": path.resolve(import.meta.dirname, "shared"),
+  //     "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+  //   },
+  // },
+  resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "client/src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
