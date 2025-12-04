6bbde83f9f530d504df572f8c25918b4557b4dda
futureshockholdings-creator <futureshockholdings@gmail.com>
2025-12-04 06:07:47 -0500

    Logout fixed

--- vite.config.ts
+++ vite.config.ts
@@ -23,9 +23,11 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+   publicDir: path.resolve(import.meta.dirname, "client/public"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
+    copyPublicDir: true
   },
   server: {
     fs: {
